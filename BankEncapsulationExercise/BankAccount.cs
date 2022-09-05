97abbf90b4ed7de89bea6c7e448e503eebb96c8f
Xavier Barnes <xdbcoder44@gmail.com>
2022-09-05 18:57:41 -0400

    Add project files.

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double Balance = 0;
+
+        public void Deposit(double makeDeposit)
+        {
+            Balance += makeDeposit;  
+        }
+
+        public void GetBalance(double amount)
+        {
+            Balance = amount;
+        }
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using BankEncapsulationExercise;
+using System;
+using System.Collections.Generic;
+
+namespace BankEncapsulaption
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            account.Deposit(400);
+
+            account.GetBalance(50);
+        }
+    }
+}
